6958f395bb9aabc25babfa26044700cb6d9fc914
Ricky Laishram <rickylaishram@gmail.com>
2014-03-01 03:56:30 -0500

    config-sample

---
+++ config-sample.php
@@ -0,0 +1,15 @@
+<?php
+	$_db["host"] = "";
+	$_db["user"] = "";
+	$_db["pass"] = "";
+	$_db["database"] = "";
+	$_config_vars['base_ssl_url'] = "";
+	$_config_vars['base_ssl_url_this'] = "";
+	$_config_vars["facebook"]["appId"] = "";
+	$_config_vars["facebook"]["secret"] = "";
+
+	$_config_vars["campaign"]["secret1"] = 1;
+	$_config_vars["campaign"]["secret2"] = 1;
+	$_config_vars["campaign"]["secret3"] = 1;
+	$_config_vars["campaign"]["secret4"] = 1;
+?>
