43e3c0aefe444029575f7db393b23bd4f45aa194
Ricky Laishram <rickylaishram@gmail.com>
2014-03-02 18:30:32 -0500

    API response functions

--- application/controllers/api.php
+++ application/controllers/api.php
@@ -10,30 +10,48 @@ class Api extends CI_Controller {
 	*/
 
 	function register() {
-		$clientId = $this->input->get_request_header('Client-Id', True);
+		$client = $this->input->post('id', True);
 		$email = $this->input->post('email', true);
 		$password = $this->input->post('password', true);
 
-		var_dump($clientId);
-		var_dump($email);
-		var_dump($password);
-
-		if($clientId && $password && $email) {
+		if($client && $password && $email) {
 			$this->load->model('client_model', 'client');
 			
-			if($this->client->isValid($clientId)) {
-				$data['status'] = true;
-				$data['data'] = array('reason' => 'valid clientid');
-				
-				$this->load->view('api/response', $data);
+			if($this->client->isValid($client)) {
+				$this->_response_success(array());
 			} else {
-				$data['status'] = false;
-				$data['data'] = array('reason' => 'Invalid clientid');
-				
-				$this->load->view('api/response', $data);
+				$this->_response_error_invalid_id();
 			}
+		} else {
+			$this->_response_error_missing_parameters();
 		}
 
+	}
+
+	/*
+	* Sends success resonse
+	* @params array data to be sent to client
+	*/
+	function _response_success($vars) {
+		$data['status'] = true;
+		$data['data'] = $vars;
+		$this->load->view('api/response', $data);
+	}
+
+	/*
+	* Error responses
+	*/
+
+	function _response_error_missing_parameters() {
+		$data['status'] = false;
+		$data['data'] = array('reason' => 'Missing parameters');
+		$this->load->view('api/response', $data);
+	}
+
+	function _response_error_invalid_id() {
+		$data['status'] = false;
+		$data['data'] = array('reason' => 'Invalid id');
+		$this->load->view('api/response', $data);
 	}
 
 }
