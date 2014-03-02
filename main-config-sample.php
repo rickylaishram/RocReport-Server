492d031d2edca469eed30787da64811fbbe1ad11
Ricky Laishram <rickylaishram@gmail.com>
2014-03-01 20:25:08 -0500

    RocReport v2

---
+++ main-config-sample.php
@@ -0,0 +1,20 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+	/* Sample file for main-config */
+
+	<?php
+	/* Keep all the secrets here */
+
+	define('RR_ENVIRONMENT', 'production');
+
+
+
+
+	/*
+	* Database
+	*/
+	define('RR_DB_HOST', '');
+	define('RR_DB_USER', '');
+	define('RR_DB_PASSWORD', '');
+	define('RR_DB_NAME', '');
+	define('RR_DB_DRIVER', '');
+	define('RR_DB_PREFIX', '');
