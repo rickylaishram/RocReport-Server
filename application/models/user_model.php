67444300481f48b32110d27b63b687af95a1dd7c
Ricky Laishram <rickylaishram@gmail.com>
2014-03-02 19:41:47 -0500

    Fixed wrong class name

--- application/models/user_model.php
+++ application/models/user_model.php
@@ -1,6 +1,6 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Client_model extends CI_Model {
+class User_model extends CI_Model {
 
 	function __construct() {
 		$this->table = $this->config->item('table');
