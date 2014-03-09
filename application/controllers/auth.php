3927f7c3d3cf0d64d63e75fb8b1135f617fa7e89
Ricky Laishram <rickylaishram@gmail.com>
2014-03-09 17:35:37 -0400

    Login controller and view

--- application/controllers/auth.php
+++ application/controllers/auth.php
@@ -41,7 +41,51 @@ class Auth extends CI_Controller {
 			// Redirect user to main page if logged in
 			header('Location: '.base_url());
 		}
-		
+	}
+
+	function login() {
+		$this->load->model('auth_model', 'auth');
+		$email = $this->auth->isLoggedIn();
+
+		if(!$email) {
+			$email = $this->input->post('email', true);
+			$pass = $this->input->post('pass', true);
+			
+			if($email && $pass) {
+				$this->load->model('user_model', 'user');
+
+				if(!$this->user->exist($email)) {
+					$this->load->model('auth_model', 'auth');
+					$user = $this->user->get($email);
+					$browser = $this->config->item('browser');
+
+					if($this->auth->hash($pass, $user->salt) == $user->password) {
+						$token = $this->auth->generateToken($email, $browser['id']);
+
+						$this->session->set_userdata($browser['cookie']['auth'], $token);
+						header('Location: '.base_url());
+
+					} else {
+						$data['page_title'] = 'Register | RocReport';
+						$data['error'] = true;
+
+						$this->load->view('app/header', $data);
+						$this->load->view('auth/register', $data);
+						$this->load->view('app/footer', $data);
+					}
+				}
+			} else {
+				$data['page_title'] = 'Register | RocReport';
+				$data['error'] = false;
+
+				$this->load->view('app/header', $data);
+				$this->load->view('auth/register', $data);
+				$this->load->view('app/footer', $data);
+			}
+		} else {
+			// Redirect user to main page if logged in
+			header('Location: '.base_url());
+		}
 	}
 
 }
