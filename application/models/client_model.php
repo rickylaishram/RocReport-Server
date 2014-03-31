c63b0096bd1609c3ea0a848a36101fdbe4953dac
Ricky Laishram <rickylaishram@gmail.com>
2014-03-31 01:40:59 -0400

    Client id check and Rate limit

--- application/models/client_model.php
+++ application/models/client_model.php
@@ -20,4 +20,37 @@ class Client_model extends CI_Model {
 			return false;
 		}
 	}
+
+	function log_request($clientid, $method, $ip) {
+		$data = array(
+				'client_id' => $clientid,
+				'time' => time(),
+				'method' => $method,
+				'ip' => $ip
+			);
+		$this->db->insert($this->table['api_request'], $data);
+	}
+
+	function check_rate_limit($clientid) {
+		// Get the rate limit allowed for the client
+		$this->db->where('id', $clientid);
+		$query = $this->db->get($this->table['client']);
+
+		$rate = $query->row()->rate_limit;
+
+		// Null means unlimited
+		if(is_null($rate)) {
+			return true;
+		} else {
+			$this->db->where('client_id', $clientid);
+			$this->db->where('time < ', (time() - 3600)); // API requests from the last 60 min
+			$query = $this->db->get($this->table['api_request']);
+
+			if($query->num_rows() < $rate) {
+				return true;
+			} else {
+				return false;
+			}
+		}
+	}
 }
