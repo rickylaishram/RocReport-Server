343110f74fe76a65be2cd52fc45a803b997d1763
Ricky Laishram <rickylaishram@gmail.com>
2014-04-05 18:58:43 -0400

    Show/hide button according to closed status

--- application/views/admin/content.php
+++ application/views/admin/content.php
@@ -13,15 +13,23 @@
 
 					<!-- Reports Detail -->
 					<div class="col-md-9 report-details">
-						<div id="report-details-category" class="row report-details-big"></div>
-						<div id="report-details-address" class="row"></div>
+						<div class="row report-details-big">
+							<div id="report-details-category" ></div>
+						</div>
+						<div class="row">
+							<div id="report-details-address"></div>
+						</div>
 						<div class="row" class="report-details-small">
 							<div id="report-details-date" class="col-md-4"></div>
 							<div id="report-details-score" class="col-md-4"></div>
 							<div id="report-details-vote" class="col-md-4"></div>
 						</div>
-						<div id="report-details-map" class="row"></div>
-						<div id="report-details-updates" class="row"></div>
+						<div class="row">
+							<div id="report-details-map" ></div>
+						</div>
+						<div class="row">
+							<div id="report-details-updates" ></div>
+						</div>
 						<div id="report-details-buttons" class="row">
 							<button type="button" class="btn btn-success" id="report-btn-open">Open Report</button>
 							<button type="button" class="btn btn-danger" id="report-btn-close">Close Report</button>
