4d84d7ab6ca73a3d47147c2a1951261cb2a347b7
Ricky Laishram <rickylaishram@gmail.com>
2014-04-05 13:24:30 -0400

    Admin controller

--- application/controllers/admin.php
+++ application/controllers/admin.php
@@ -2,6 +2,17 @@
 
 class Admin extends CI_Controller {
 
+	public __construct() {
+		$this->load_model('auth_model', 'auth');
+		if(!$this->auth->isLoggedIn() || !$this->auth->isAdmin(null, null, null, null)) {
+			$this->output->set_header('Location: '.base_url());
+			$this->output->set_status_header('302');
+			$this->output->_display();
+		}
+	}
 
+	public index() {
+		echo "Hello";
+	}
 
 }
