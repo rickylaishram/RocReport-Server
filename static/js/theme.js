fd9081ad529829b139a3d967d9a9d2fc9de2c2db
Ricky Laishram <rickylaishram@gmail.com>
2014-04-05 18:01:28 -0400

    Position spelling typo

--- static/js/theme.js
+++ static/js/theme.js
@@ -9,9 +9,9 @@ $(document).resize(function() {
 function set_admin_nav_height() {
 	if($(window).width() > 768) {
 		$('.admin-menu').height($(window).height() - 50);
-		$('.admin-menu').css({postion: 'fixed'});
+		$('.admin-menu').css({position: 'fixed'});
 	} else {
 		$('.admin-menu').height('auto');
-		$('.admin-menu').css({postion: 'relative'});
+		$('.admin-menu').css({position: 'relative'});
 	}
 }
