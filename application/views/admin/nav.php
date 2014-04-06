b9f0647804380995f2a6c7b4b80e38fca3a30650
Ricky Laishram <rickylaishram@gmail.com>
2014-04-06 03:42:01 -0400

    Nav bar changes

--- application/views/admin/nav.php
+++ application/views/admin/nav.php
@@ -3,10 +3,10 @@
 		<!-- List of admin categories -->
 		<div class="col-md-2 admin-menu">
 			<ul class="list-group">
-				<li class="list-group-item admin-menu-item" data-id="1">Overview</li>
+				<!--<li class="list-group-item admin-menu-item" data-id="1">Overview</li>-->
 				<li class="list-group-item admin-menu-item" data-id="2">Open Reports</li>
 				<li class="list-group-item admin-menu-item" data-id="3">Closed Reports</li>
-				<li class="list-group-item admin-menu-item" data-id="4">Notifications</li>
+				<!--<li class="list-group-item admin-menu-item" data-id="4">Notifications</li>-->
 			</ul>
 		</div>
 		<div class="col-md-2"></div>
