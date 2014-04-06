3eb08eb4d77c1479b8d025f8581d984e8526dfda
Ricky Laishram <rickylaishram@gmail.com>
2014-04-06 04:05:51 -0400

    User menu

--- application/views/user/nav.php
+++ application/views/user/nav.php
@@ -1,12 +1,12 @@
 <div class="container-fluid">
 	<div class="row">
 		<!-- List of admin categories -->
-		<div class="col-md-2 admin-menu">
+		<div class="col-md-2 user-menu">
 			<ul class="list-group">
-				<!--<li class="list-group-item admin-menu-item" data-id="1">Overview</li>-->
-				<li class="list-group-item admin-menu-item" data-id="2">Nearby Reports</li>
-				<li class="list-group-item admin-menu-item" data-id="3">My Reports</li>
-				<!--<li class="list-group-item admin-menu-item" data-id="4">Notifications</li>-->
+				<!--<li class="list-group-item user-menu-item" data-id="1">Overview</li>-->
+				<li class="list-group-item user-menu-item" data-id="2">Nearby Reports</li>
+				<li class="list-group-item user-menu-item" data-id="3">My Reports</li>
+				<!--<li class="list-group-item user-menu-item" data-id="4">Notifications</li>-->
 			</ul>
 		</div>
 		<div class="col-md-2"></div>
