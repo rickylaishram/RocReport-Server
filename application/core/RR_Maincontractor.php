5f3fd1137d55de4a9b543b9957a240687f7ffc13
Ricky Laishram <rickylaishram@gmail.com>
2014-09-13 15:59:03 -0400

    debug

--- application/core/RR_Maincontractor.php
+++ application/core/RR_Maincontractor.php
@@ -14,11 +14,15 @@ class RR_Maincontractor extends RR_Mainlogin {
 		
 		$this->load->model('auth_model', 'auth');
 
+		var_dump($this->auth->isContractor($this->user_data['email']));
+
 		if(!$this->auth->isContractor($this->user_data['email'])) {
 			$this->output->set_header('Location: '.base_url());
 			$this->output->set_status_header('302');
 			$this->output->_display();
 		}
+
+		var_dump('hsgdfjhs');
 
 		$this->load->model('contractor_model', 'con');
 		$this->contractor_data = $this->con->getData($this->user_data['email']);
