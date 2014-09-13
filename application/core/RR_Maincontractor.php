ce66c9fd817865d651e5f3e24ef86ebddefa6427
Ricky Laishram <rickylaishram@gmail.com>
2014-09-13 16:01:20 -0400

    Redirect if not contractor

--- application/core/RR_Maincontractor.php
+++ application/core/RR_Maincontractor.php
@@ -14,18 +14,13 @@ class RR_Maincontractor extends RR_Mainlogin {
 		
 		$this->load->model('auth_model', 'auth');
 
-		var_dump($this->auth->isContractor($this->user_data['email']));
-
 		if(!$this->auth->isContractor($this->user_data['email'])) {
 			$this->output->set_header('Location: '.base_url());
 			$this->output->set_status_header('302');
 			$this->output->_display();
-			var_dump('adtsfyavfeyad!!@@');
-		} else {
-			var_dump('agsfdghsafdjha');
+			
+			exit();
 		}
-
-		var_dump('hsgdfjhs');
 
 		$this->load->model('contractor_model', 'con');
 		$this->contractor_data = $this->con->getData($this->user_data['email']);
