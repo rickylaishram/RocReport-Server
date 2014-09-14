35f770da48ff283526b113f72510480483a1275c
Ricky Laishram <rickylaishram@gmail.com>
2014-09-13 20:45:33 -0400

    Bootflat

--- application/views/app/header.php
+++ application/views/app/header.php
@@ -12,19 +12,22 @@
 		<!-- Bootstrap core CSS -->
 		<link href="<?=base_url(); ?>static/css/bootstrap.min.css" rel="stylesheet">
 
-		<!-- Custom styles for this template -->
-		<link href="<?=base_url(); ?>static/css/style.css" rel="stylesheet">
+		<link rel="stylesheet" type="text/css" href="<?=base_url(); ?>static/css/bootstrap-select.min.css">
+		<link href="<?=base_url(); ?>static/css/bootflat.min.css" rel="stylesheet">
 
 		<link href='//fonts.googleapis.com/css?family=Ubuntu:300,400,500,700,300italic,400italic,500italic,700italic' rel='stylesheet' type='text/css'>
 		<link rel="stylesheet" href="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/themes/smoothness/jquery-ui.css" />
+		
+		<!-- Custom styles for this template -->
+		<link href="<?=base_url(); ?>static/css/style.css" rel="stylesheet">
+
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
 		<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/jquery-ui.min.js"></script>
 		<script src="<?=base_url(); ?>static/js/d3.min.js"></script>
 		<script src="<?=base_url(); ?>static/js/bootstrap.min.js"></script>
 		<script src="<?=base_url(); ?>static/js/theme.js"></script>
-		<?php if( $this->router->class == "add_report" ) { ?>
-		<script type="text/javascript" src="<?=base_url(); ?>static/js/bootstrap-select.min.js"></script>
-		<script type="text/javascript" src="<?=base_url(); ?>static/js/SimpleAjaxUploader.js"></script>
-		<link rel="stylesheet" type="text/css" href="<?=base_url(); ?>static/css/bootstrap-select.min.css">
-		<?php } ?>
+		<?php if( $this->router->class == "add_report" ): ?>
+			<script type="text/javascript" src="<?=base_url(); ?>static/js/bootstrap-select.min.js"></script>
+			<script type="text/javascript" src="<?=base_url(); ?>static/js/SimpleAjaxUploader.js"></script>
+		<?php endif; ?>
 	</head>
