89cfbc9f91b22fb507528a890692339119a6d17f
Ricky Laishram <rickylaishram@gmail.com>
2014-09-14 10:26:35 -0400

    login page styling

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -3,18 +3,39 @@
 		<div class="col-lg-4 col-md-4 col-sm-12 col-xs-12 col-lg-offset-4 col-md-offset-4">
 			<div class="panel panel-default">
 				<div class="panel-heading panel-header-main">Login</div>
+				
 				<div class="panel-body">
-					<form class="form-signin" role="form" action="" method="post">
-						<div id="message">
-							<?php if($error) : ?>
-								<div class="alert alert-danger">Invalid login credentials.</div>
-							<?php endif; ?>
+					<div id="message">
+						<?php if($error) : ?>
+							<div class="alert alert-danger">Invalid login credentials.</div>
+						<?php endif; ?>
+					</div>
+
+					<form class="form-horizontal" role="form" action="" method="post">
+						<div class="form-group">
+							<label for="email" class="col-lg-3 control-label">Email</label>
+							<div class="col-lg-9">
+								<input type="email" class="form-control" placeholder="Email address" required id="email" name="email">
+							</div>
 						</div>
-						<input type="email" class="form-control" placeholder="Email address" required id="email" name="email">
-						<input type="password" class="form-control" placeholder="Password" required id="pass" name="pass">
-						<button class="btn btn-lg btn-primary btn-block" id="btn_submit" type="submit" disabled="disabled">Login</button>
+
+						<div class="form-group">
+							<label for="pass" class="col-lg-3 control-label">Email</label>
+							<div class="col-lg-9">
+								<input type="password" class="form-control" placeholder="Password" required id="pass" name="pass">
+							</div>
+						</div>
+
+						<div class="form-group">
+							<label for="inputEmail" class="col-lg-3 control-label"></label>
+							<div class="col-lg-9">
+								<button class="btn btn-lg btn-primary btn-block" id="btn_submit" type="submit" disabled="disabled">Login</button>
+							</div>
+						</div>
+						
 					</form>
 				</div>
+
 			</div>
 		</div>
 	</div>
