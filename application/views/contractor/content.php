670aa9e55b205d96bcf6b63e924ebd5f47e0e246
Ricky Laishram <rickylaishram@gmail.com>
2014-09-14 03:06:29 -0400

    materia. css

--- application/views/contractor/content.php
+++ application/views/contractor/content.php
@@ -1,6 +1,6 @@
 <div class="container-fluid">
 	<div class="row">
-		<div class="col-lg-5 col-md-5 col-sm-12 col-xs-12 col-main">
+		<div class="col-lg-5 col-md-5 col-sm-12 col-xs-12">
 			<div class="panel panel-default">
 				<div class="panel-heading panel-header-main">Search</div>
 				<div class="panel-body">
@@ -9,7 +9,7 @@
 			</div>
 		</div>
 
-		<div class="col-lg-5 col-md-5 col-sm-12 col-xs-12 col-main">
+		<div class="col-lg-5 col-md-5 col-sm-12 col-xs-12">
 			<div class="panel panel-default">
 				<div class="panel-heading panel-header-main">Bids</div>
 				<div class="panel-body">
@@ -18,7 +18,7 @@
 			</div>
 		</div>
 
-		<div class="col-lg-2 col-md-2 col-sm-12 col-xs-12 col-main">
+		<div class="col-lg-2 col-md-2 col-sm-12 col-xs-12">
 			<div class="panel panel-default">
 				<div class="panel-heading panel-header-main">Profile</div>
 				<div class="panel-body">
