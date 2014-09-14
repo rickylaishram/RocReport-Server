f3f9bfbe635b47242f284becd22e74597c5a664a
Ricky Laishram <rickylaishram@gmail.com>
2014-09-13 20:49:32 -0400

    debug

--- application/controllers/contractor/jobs.php
+++ application/controllers/contractor/jobs.php
@@ -9,10 +9,15 @@ class Jobs extends RR_Maincontractor {
 		$this->data['page_data']['page_title'] = 'Contractor - Jobs | RocReport';
 		$this->data['page_data']['page_id'] = 8;
 
-		$category = $this->input->get('category', true);
-		$latitude = $this->input->get('latitude', true);
-		$longitude = $this->input->get('longitude', true);
-		$radius = $this->input->get('radius', true);
+		$category = $this->input->get('cat', true);
+		$latitude = $this->input->get('lat', true);
+		$longitude = $this->input->get('lng', true);
+		$radius = $this->input->get('rad', true);
+
+		var_dump($category);
+		var_dump($latitude);
+		var_dump($longitude);
+		var_dump($radius);
 
 		//if($category && $latitude && $longitude && $radius)
 			$this->data['data']['jobs'] = $this->job->searchNearbyType($category, $latitude, $longitude, $radius);
