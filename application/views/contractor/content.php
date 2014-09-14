7348632be93b77ed7ece4af99551359a3f731d8b
Ricky Laishram <rickylaishram@gmail.com>
2014-09-14 02:32:18 -0400

    styling

--- application/views/contractor/content.php
+++ application/views/contractor/content.php
@@ -1,27 +1,27 @@
 <div class="container-fluid">
 	<div class="row">
 		<div class="col-lg-5 col-md-5 col-sm-12 col-xs-12 col-main">
-			<div class="row col-header">
-				<div class="ccol-lg-12 col-md-12 col-sm-12 col-xs-12">Search</div>
+			<div class="row">
+				<div class="ccol-lg-12 col-md-12 col-sm-12 col-xs-12 col-header">Search</div>
 			</div>
-			<div class="row col-content">
-				<div class="ccol-lg-12 col-md-12 col-sm-12 col-xs-12"></div>
+			<div class="row">
+				<div class="ccol-lg-12 col-md-12 col-sm-12 col-xs-12 col-content"></div>
 			</div>
 		</div>
 		<div class="col-lg-5 col-md-5 col-sm-12 col-xs-12 col-main">
-			<div class="row col-header">
-				<div class="ccol-lg-12 col-md-12 col-sm-12 col-xs-12">Bids</div>
+			<div class="row">
+				<div class="ccol-lg-12 col-md-12 col-sm-12 col-xs-12 col-header">Bids</div>
 			</div>
-			<div class="row col-content">
-				<div class="ccol-lg-12 col-md-12 col-sm-12 col-xs-12"></div>
+			<div class="row">
+				<div class="ccol-lg-12 col-md-12 col-sm-12 col-xs-12 col-content"></div>
 			</div>
 		</div>
 		<div class="col-lg-2 col-md-2 col-sm-12 col-xs-12 col-main">
-			<div class="row col-header">
-				<div class="ccol-lg-12 col-md-12 col-sm-12 col-xs-12">Profile</div>
+			<div class="row">
+				<div class="ccol-lg-12 col-md-12 col-sm-12 col-xs-12 col-header">Profile</div>
 			</div>
-			<div class="row col-content">
-				<div class="ccol-lg-12 col-md-12 col-sm-12 col-xs-12"></div>
+			<div class="row">
+				<div class="ccol-lg-12 col-md-12 col-sm-12 col-xs-12 col-content"></div>
 			</div>
 		</div>
 	</div>
