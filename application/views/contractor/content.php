8c69ad459436485cb908e7f05d1bfaf16a3e2f92
Ricky Laishram <rickylaishram@gmail.com>
2014-09-14 02:39:34 -0400

    styling

--- application/views/contractor/content.php
+++ application/views/contractor/content.php
@@ -1,27 +1,29 @@
 <div class="container-fluid">
 	<div class="row">
 		<div class="col-lg-5 col-md-5 col-sm-12 col-xs-12 col-main">
-			<div class="row">
-				<div class="ccol-lg-12 col-md-12 col-sm-12 col-xs-12 col-header">Search</div>
-			</div>
-			<div class="row">
-				<div class="ccol-lg-12 col-md-12 col-sm-12 col-xs-12 col-content"></div>
+			<div class="panel panel-default">
+				<div class="panel-heading panel-header-main">Search</div>
+				<div class="panel-body">
+					Basic panel example
+				</div>
 			</div>
 		</div>
+
 		<div class="col-lg-5 col-md-5 col-sm-12 col-xs-12 col-main">
-			<div class="row">
-				<div class="ccol-lg-12 col-md-12 col-sm-12 col-xs-12 col-header">Bids</div>
-			</div>
-			<div class="row">
-				<div class="ccol-lg-12 col-md-12 col-sm-12 col-xs-12 col-content"></div>
+			<div class="panel panel-default">
+				<div class="panel-heading panel-header-main">Bids</div>
+				<div class="panel-body">
+					Basic panel example
+				</div>
 			</div>
 		</div>
+
 		<div class="col-lg-2 col-md-2 col-sm-12 col-xs-12 col-main">
-			<div class="row">
-				<div class="ccol-lg-12 col-md-12 col-sm-12 col-xs-12 col-header">Profile</div>
-			</div>
-			<div class="row">
-				<div class="ccol-lg-12 col-md-12 col-sm-12 col-xs-12 col-content"></div>
+			<div class="panel panel-default">
+				<div class="panel-heading panel-header-main">Profile</div>
+				<div class="panel-body">
+					Basic panel example
+				</div>
 			</div>
 		</div>
 	</div>
