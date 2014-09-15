879728f942e1006c6ba32afa757b1ec7073b0981
Ricky Laishram <rickylaishram@gmail.com>
2014-09-14 20:32:02 -0400

    fetch categories url

--- application/views/contractor/content.php
+++ application/views/contractor/content.php
@@ -82,5 +82,7 @@
 	r.contractor.data.id = <?=json_encode($auth['id']); ?>;
 	r.contractor.data.nonce = <?=json_encode($auth['nonce']); ?>;
 
+	r.contractor.url.fetchCategories = <?=base_url(); ?>'/private_api/category/fetch';
+
 	r.contractor.init();
 </script>
