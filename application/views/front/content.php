fffab21fa0e1ec810af56507e8f741bf2268d56b
Ricky Laishram <rickylaishram@gmail.com>
2014-11-01 21:50:05 -0400

    fixed class

--- application/views/front/content.php
+++ application/views/front/content.php
@@ -6,14 +6,14 @@
 				<div class="col-md-4 col-lg-4 hidden-xs hidden-sm">
 					<div class="glyphicon glyphicon-phone"></div>
 				</div>
-				<div class="col-md-4 col-lg-4 col-xs-12 col-sm-12">
+				<div class="col-md-8 col-lg-8 col-xs-12 col-sm-12">
 					<p>Take a photo</p>
 					<p>Describe the type of job</p>
 					<p>Post on Rocreport</p>
 				</div>
 			</div>
 			<div class="row">
-				<div class="col-md-4 col-lg-4 col-xs-12 col-sm-12">
+				<div class="col-md-8 col-lg-8 col-xs-12 col-sm-12">
 					<p>Nearby contractors sees your job</p>
 					<p>Contractors bid on the jobs</p>
 					<p>You select the contractor</p>
