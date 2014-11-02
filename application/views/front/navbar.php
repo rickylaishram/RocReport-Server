402c469bddee91071b4a56ddf52e10876eaa67c8
Ricky Laishram <rickylaishram@gmail.com>
2014-11-01 21:47:57 -0400

    Changed file name

--- application/views/front/navbar.php
+++ application/views/front/navbar.php
@@ -2,7 +2,7 @@
 
 	<!-- Fixed navbar -->
 		<div class="navbar navbar-default navbar-fixed-top" role="navigation">
-			<div class="container-fluid">
+			<div class="container">
 				<div class="navbar-header">
 					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
 						<span class="sr-only">Toggle navigation</span>
@@ -14,10 +14,10 @@
 				</div>
 				<div class="navbar-collapse collapse">
 					<ul class="nav navbar-nav">
-						<li class="front-nav" data-content="front-what">What is Rocreport?</li>
-						<li class="front-nav" data-content="front-signup">How do I sign up?</li>
-						<li class="front-nav" data-content="front-app">Mobile App</li>
-						<li class="front-nav" data-content="front-api">API</li>
+						<li class="front-nav" data-content="front-what"><a>What is Rocreport?</a></li>
+						<li class="front-nav" data-content="front-signup"><a>How do I sign up?</a></li>
+						<li class="front-nav" data-content="front-app"><a>Mobile App</a></li>
+						<li class="front-nav" data-content="front-api"><a>API</a></li>
 					</ul>
 					<!--<ul class="nav navbar-nav navbar-right">
 						<?php if($is_logged_in): ?>
